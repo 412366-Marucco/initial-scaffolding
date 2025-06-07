eee4bd30e63236a2fa55b0a00f4f64f14ddf5de9
412366_Marucco <412366@tecnicatura.frc.utn.edu.ar>
2025-06-07 16:11:55 -0300

    Fix MappersConfig decorator.

--- src/main/java/ar/edu/utn/frc/tup/piii/config/MappersConfig.java
+++ src/main/java/ar/edu/utn/frc/tup/piii/config/MappersConfig.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class MappersConfig {
 
     @Bean
